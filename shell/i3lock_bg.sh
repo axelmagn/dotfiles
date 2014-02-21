1bae2620474402b065bc9037c459aebb1354f134
Axel Magnuson <axelmagn@gmail.com>
2014-02-21 02:56:32 -0700

    bugfix i3lock prettify

--- shell/i3lock_bg.sh
+++ shell/i3lock_bg.sh
@@ -1,5 +1,5 @@
 if hash import 2>/dev/null; then
     if hash convert 2>/dev/null; then
-        alias i3lock="import -window root $HOME/.i3lockbg.png; convert --scale 10% --scale 1000% $HOME/.i3lockbg $HOME/.i3lockbg"
+        alias i3lock="import -window root $HOME/.i3lockbg.png && convert -scale 10% -scale 1000% $HOME/.i3lockbg.png $HOME/.i3lockbg.png; i3lock -i $HOME/.i3lockbg.png"
     fi
 fi
