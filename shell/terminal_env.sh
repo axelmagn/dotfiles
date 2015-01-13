2423ac061262cbe29d9c66968aa0f69cece888d9
Axel Magnuson <axelmagn@gmail.com>
2015-01-13 15:46:42 -0700

    modify set term and i3 borders

--- shell/terminal_env.sh
+++ shell/terminal_env.sh
@@ -1,7 +1,9 @@
 if [ "$COLORTERM" == "gnome-terminal" ] || [ "$COLORTERM" == "xfce4-terminal" ]
 then
     TERM=xterm-256color
-    elif [ "$COLORTERM" == "rxvt-xpm" ]
-    then
-        TERM=rxvt-256color
+elif [ "$COLORTERM" == "rxvt-xpm" ]
+then
+    TERM=rxvt-256color
+else
+    TERM=xterm-16color
 fi
