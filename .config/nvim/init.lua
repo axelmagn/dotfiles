8dc6db6593f8494c94500fae2c00a8c1eeb34dcf
Axel Magnuson <axelmagn@gmail.com>
2025-07-25 19:11:14 -0700

    add godot support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,14 @@
 --[[
-
+--
 =====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+======================= AXEL'S NEOVIM CONFIG ========================
 =====================================================================
 ========                                    .-----.          ========
 ========         .----------------------.   | === |          ========
 ========         |.-""""""""""""""""""-.|   |-----|          ========
 ========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||     COOL SHIT      ||   |-----|          ========
 ========         ||                    ||   | === |          ========
 ========         ||                    ||   |-----|          ========
 ========         ||:Tutor              ||   |:::::|          ========
@@ -20,69 +21,22 @@
 =====================================================================
 =====================================================================
 
-What is Kickstart?
+----------------------------------------------------------------------------------------------------
+-- FRONTMATTER
+----------------------------------------------------------------------------------------------------
 
-  Kickstart.nvim is *not* a distribution.
+Reference Reading:
+  - :Tutor
+  - :help help
+  - :help lua-guide
+  - https://learnxinyminutes.com/docs/lua/
+  - :checkhealth
 
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
+
+----------------------------------------------------------------------------------------------------
+-- OPTIONS
+----------------------------------------------------------------------------------------------------
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -573,13 +527,11 @@ require("lazy").setup({
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					-- map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					-- map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
@@ -705,7 +657,11 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {},
+				cmake = {},
 				zls = {},
+				-- gdscript = {},
+				-- gdshader_lsp = {},
+
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
@@ -767,6 +723,17 @@ require("lazy").setup({
 					end,
 				},
 			})
+
+			-- GDScript Config
+			-- can't add 'gdscript' to servers because it's not listed in Mason.
+			local gdscript_config = {
+				capabilities = capabilities,
+				settings = {},
+			}
+			if vim.fn.has("win32") == 1 then
+				gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
+			end
+			require("lspconfig").gdscript.setup(gdscript_config)
 		end,
 	},
 
@@ -929,11 +896,7 @@ require("lazy").setup({
 				},
 			})
 
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight")
-			-- vim.cmd.colorscheme("tokyonight-night")
-			-- vim.cmd.colorscheme("elflord")
+			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 
@@ -1020,6 +983,16 @@ require("lazy").setup({
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 
+	-- in-editor terminal manager (useful for windows where tmux is less viable)
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup()
+			vim.keymap.set("n", "<leader>tt", vim.cmd.ToggleTerm, { desc = "[T]oggle [T]erminal" })
+			vim.keymap.set({ "n", "i", "t" }, "<C-\\>", vim.cmd.ToggleTerm, { desc = "[T]oggle [T]erminal" })
+		end,
+	},
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
@@ -1029,12 +1002,12 @@ require("lazy").setup({
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+	require("kickstart.plugins.debug"),
+	require("kickstart.plugins.indent_line"),
+	-- require("kickstart.plugins.lint"),
+	-- require("kickstart.plugins.autopairs"),
+	require("kickstart.plugins.neo-tree"),
+	-- require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
@@ -1067,6 +1040,16 @@ require("lazy").setup({
 		},
 	},
 })
+
+-- enable neovim to be the external editor for godot, if the cwd has a project.godot file
+if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
+	local addr = "./godot.pipe"
+	if vim.fn.has("win32") == 1 then
+		-- windows can't pipe so use localhost
+		addr = "127.0.0.1:6004"
+	end
+	vim.fn.serverstart(addr)
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
