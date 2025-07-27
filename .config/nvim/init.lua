88bd5e0955d46b7cd505fbe38472ce5f30f77a48
Axel Magnuson <axelmagn@gmail.com>
2025-07-26 23:05:41 -0700

    tweak windows nvim behavior

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -988,7 +988,25 @@ require("lazy").setup({
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = function()
+			-- powershell requires special config on windows
+			-- these must be set prior to toggleterm setup
+			if vim.fn.has("win32") == 1 then
+				local powershell_options = {
+					shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+					shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+					shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+					shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+					shellquote = "",
+					shellxquote = "",
+				}
+
+				for option, value in pairs(powershell_options) do
+					vim.opt[option] = value
+				end
+			end
+
 			require("toggleterm").setup()
+
 			vim.keymap.set("n", "<leader>tt", vim.cmd.ToggleTerm, { desc = "[T]oggle [T]erminal" })
 			vim.keymap.set({ "n", "i", "t" }, "<C-\\>", vim.cmd.ToggleTerm, { desc = "[T]oggle [T]erminal" })
 		end,
